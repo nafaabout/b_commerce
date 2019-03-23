51f798c0fa17e84f35243e2dad94ad68ebcd4952
Nafaa Boutefer <nafaabout@gmail.com>
2019-03-23 15:59:29 +0100

    lib/b_commerce.rb requires rb files in lib/b_commerce from

--- lib/b_commerce.rb
+++ lib/b_commerce.rb
@@ -1,8 +1,11 @@
+$:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__))
+
+require 'excon'
+require 'json'
+
 require 'b_commerce/version'
 require 'b_commerce/base'
 require 'b_commerce/products'
 
 module BCommerce
-  class Error < StandardError; end
-  # Your code goes here...
 end
