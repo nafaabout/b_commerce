78d260a9802f97f55581cc569afe3db713bfde8b
Nafaa Boutefer <nafaabout@gmail.com>
2019-04-10 14:11:28 +0100

    Adds specs for CategoriesList class

--- lib/b_commerce/catalog/categories_list.rb
+++ lib/b_commerce/catalog/categories_list.rb
@@ -4,6 +4,25 @@ module BCommerce
       PATH = '/catalog/categories'
       API_VERSION = :v3
 
+      QUERY_PARAMS = {
+        enum: {
+          is_visible: [true, false, 1, 0]
+        },
+        non_enum: {
+          name: String,
+          parent_id: Integer,
+          page_title: String,
+          keyword: String,
+          page: Integer,
+          limit: Integer,
+          include_fields: String,
+          exclude_fields: String,
+          categories: String
+        }
+      }
+      generate_enum_params_query_methods(params: QUERY_PARAMS[:enum])
+      generate_non_enum_params_query_methods(params: QUERY_PARAMS[:non_enum])
+
       def tree
         resp = connection.get(path: "#{path}/tree")
         response_data(response: resp)
--- spec/b_commerce/catalog/categories_list_spec.rb
+++ spec/b_commerce/catalog/categories_list_spec.rb
@@ -1,40 +1,42 @@
-module BCommerce::Catalog
-  RSpec.describe CategoriesList do
-    let(:store_hash){ rand.to_s }
-    let(:client_id){ rand.to_s }
-    let(:auth_token){ rand.to_s }
+module BCommerce
+  module Catalog
+    RSpec.describe CategoriesList do
+      let(:store_hash){ rand.to_s }
+      let(:client_id){ rand.to_s }
+      let(:auth_token){ rand.to_s }
 
-    let(:categories_list){ CategoriesList.new }
+      let(:categories_list){ CategoriesList.new }
 
-    it 'inherits from BCommerce::ResourceList' do
-      expect(CategoriesList).to be < ResourceList
-    end
-
-    specify 'PATH is /catalog/categories' do
-      expect(CategoriesList::PATH).to eq '/catalog/categories'
-    end
-
-    specify 'API_VERSION is :v3' do
-      expect(CategoriesList::API_VERSION).to be :v3
-    end
-
-    describe '#tree' do
-      before do
-        BCommerce::Base.setup(client_id: client_id, store_hash: store_hash, auth_token: auth_token)
+      it 'inherits from BCommerce::ResourceList' do
+        expect(CategoriesList).to be < ResourceList
       end
 
-      it 'returns all categories tree' do
-        categories = [{
-          "children": [],
-          "id": 58,
-          "is_visible": true,
-          "name": "add",
-          "parent_id": 0,
-          "url": "/sports/"
-        }]
+      specify 'PATH is /catalog/categories' do
+        expect(CategoriesList::PATH).to eq '/catalog/categories'
+      end
 
-        Excon.stub({ method: :get, path: "#{categories_list.path}/tree" }, { body: { data: categories }.to_json })
-        expect(categories_list.tree).to eq(categories)
+      specify 'API_VERSION is :v3' do
+        expect(CategoriesList::API_VERSION).to be :v3
+      end
+
+      describe '#tree' do
+        before do
+          BCommerce::Base.setup(client_id: client_id, store_hash: store_hash, auth_token: auth_token)
+        end
+
+        it 'returns all categories tree' do
+          categories = [{
+            "children": [],
+            "id": 58,
+            "is_visible": true,
+            "name": "add",
+            "parent_id": 0,
+            "url": "/sports/"
+          }]
+
+          Excon.stub({ method: :get, path: "#{categories_list.path}/tree" }, { body: { data: categories }.to_json })
+          expect(categories_list.tree).to eq(categories)
+        end
       end
     end
   end
