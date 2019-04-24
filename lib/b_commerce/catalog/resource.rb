8645bb5ff9f50ed8c2d484d0a840991843677e3d
Nafaa Boutefer <nafaabout@gmail.com>
2019-04-24 16:04:36 +0100

    Resource#attribute generates valid_#{attr}? method for Array attrs

--- lib/b_commerce/catalog/resource.rb
+++ lib/b_commerce/catalog/resource.rb
@@ -34,6 +34,29 @@ module BCommerce
           end
         end
 
+        def define_array_attribute(attr, options)
+          if !options[:values_type].is_a?(Class) && !options.key?(:validate_with)
+            raise ArgumentError.new('For type Array, :values_type or :validate_with arguments should be passed.')
+          end
+
+          if options[:values_type]
+            define_method("valid_#{attr}?") do
+              return false unless attributes[attr].is_a?(Array)
+              type_method = options[:values_type].to_s
+              attributes[attr].all? do |v|
+                send(type_method, v)
+                true
+              rescue StandardError
+                false
+              end
+            end
+          elsif options.key?(:validate_with)
+            define_method(:"valid_#{attr}?") do
+              send(options[:validate_with])
+            end
+          end
+        end
+
         def define_string_attribute(attr, options)
           define_method("valid_#{attr}?") do
             if(options[:length])
@@ -44,6 +67,46 @@ module BCommerce
           end
         end
 
+        def define_datetime_attribute(attr, options)
+          define_method("valid_#{attr}?") do
+            attributes[attr].is_a?(DateTime) || valid_datetime?(attributes[attr])
+          end
+        end
+
+        def valid_datetime?(value)
+          DateTime.strptime(value)
+          true
+        rescue
+          false
+        end
+
+        def define_float_attribute(attr, options)
+          define_method("valid_#{attr}?") do
+            return false unless value.is_a?(String) || value.is_a?(Numeric)
+            value = attributes[attr]
+            valid_float?(value)
+          end
+        end
+
+        def define_integer_attribute(attr, options)
+          define_method("valid_#{attr}?") do
+            return false unless value.is_a?(String) || value.is_a?(Numeric)
+            value = attributes[attr]
+            valid = valid_integer?(value)
+            if valid && options[:range]
+              valid = options[:range].include? value.to_i
+            end
+            valid
+          end
+        end
+
+        def valid_float?(value)
+          value.to_f.to_s == value.to_s
+        end
+
+        def valid_integer?(value)
+          valid_float?(value) && value.to_i == value.to_f
+        end
       end
 
 
--- spec/b_commerce/catalog/resource_spec.rb
+++ spec/b_commerce/catalog/resource_spec.rb
@@ -4,6 +4,13 @@ module BCommerce
     class SomeResource < Resource
       attribute :type, values: ['physical', 'digital']
       attribute :name, type: String, length: 1..255
+      attribute :categories, type: Array, values_type: Integer
+
+
+      private
+      def validate_custom_fields
+        true
+      end
     end
 
     RSpec.describe Resource do
@@ -57,7 +64,46 @@ module BCommerce
           end
         end
 
-        context 'FOR Array attribute'
+        context 'FOR Array attribute' do
+          it 'generates validation method' do
+            expect(some_resource).to respond_to(:valid_categories?)
+          end
+
+          context 'IF :values_type passed' do
+
+            context 'AND all values are of the given type' do
+              it 'returns true' do
+                some_resource.attributes[:categories] = [13, '24']
+                expect(some_resource.valid_categories?).to be true
+              end
+            end
+
+            context 'AND some values are not of the given type' do
+              it 'returns false' do
+                some_resource.attributes[:categories] = [13, 'f4']
+                expect(some_resource.valid_categories?).to be false
+              end
+            end
+          end
+
+          context 'IF :values_type NOT passed OR not a Class' do
+            context 'AND :validate_with is passed' do
+              it 'generated method calls method passed in :validate_with' do
+                expect(some_resource).to receive(:validate_custom_fields)
+                SomeResource.attribute(:custom_fields, type: Array, validate_with: :validate_custom_fields)
+                some_resource.valid_custom_fields?
+              end
+            end
+
+            context 'AND :validate_with is NOT passed' do
+              it 'raises an error' do
+                expect{ SomeResource.attribute(:goodies, type: Array) }.to \
+                  raise_error(ArgumentError, 'For type Array, :values_type or :validate_with arguments should be passed.')
+              end
+            end
+          end
+        end
+
         context 'FOR Boolean attribute'
         context 'FOR Integer attribute'
         context 'FOR Float attribute'
