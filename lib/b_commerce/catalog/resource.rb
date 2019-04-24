82050ef51655372cc36fabad786347a9772fbda8
Nafaa Boutefer <nafaabout@gmail.com>
2019-04-24 14:53:16 +0100

    Resource#attribute generates valid_#{attr}? method for Enum and String attrs

--- lib/b_commerce/catalog/resource.rb
+++ lib/b_commerce/catalog/resource.rb
@@ -1,4 +1,6 @@
 module BCommerce
+  BOOLEAN = [true, false, 1, 0]
+
   module Catalog
     class Resource < Base
       attr_reader :attributes
@@ -13,6 +15,38 @@ module BCommerce
       def save
 
       end
+
+      class << self
+        def attribute(attr, options = {})
+          if options[:type]
+            type = options[:type].to_s.downcase
+            send(:"define_#{type}_attribute", attr, options)
+          elsif(options[:values])
+            define_enum_attribute(attr, options)
+          end
+        end
+
+        private
+
+        def define_enum_attribute(attr, options)
+          define_method("valid_#{attr}?") do
+            options[:values].include?(attributes[attr].to_s)
+          end
+        end
+
+        def define_string_attribute(attr, options)
+          define_method("valid_#{attr}?") do
+            if(options[:length])
+              options[:length].include?(attributes[attr].to_s.length)
+            else
+              attributes[attr].is_a?(String)
+            end
+          end
+        end
+
+      end
+
+
     end
   end
 end
--- spec/b_commerce/catalog/resource_spec.rb
+++ spec/b_commerce/catalog/resource_spec.rb
@@ -2,13 +2,66 @@ module BCommerce
   module Catalog
 
     class SomeResource < Resource
-
+      attribute :type, values: ['physical', 'digital']
+      attribute :name, type: String, length: 1..255
     end
 
     RSpec.describe Resource do
 
       it 'inherits from BCommerce::Base' do
         expect(Resource).to be < Base
+      end
+
+      describe '.attribute' do
+        let(:some_resource){ SomeResource.new }
+
+        context 'FOR Enum attribute' do
+          it 'generates validation method' do
+            expect(some_resource).to respond_to(:valid_type?)
+          end
+
+          context 'IF attribute value is in the enum' do
+            specify 'generated method returns true' do
+              some_resource.attributes[:type] = 'physical'
+              expect(some_resource.valid_type?).to be true
+            end
+          end
+
+          context 'IF attribute value is not in the enum' do
+            specify 'generated method returns false' do
+              some_resource.attributes[:type] = 'not_in_enum_values'
+              expect(some_resource.valid_type?).to be false
+            end
+          end
+        end
+
+        context 'FOR string attribute' do
+          it 'generates validation method' do
+            expect(some_resource).to respond_to(:valid_name?)
+          end
+
+          context 'IF attribute value length is within length range' do
+            specify 'valid_#{attr}? method returns true' do
+              some_resource.attributes[:name] = 'not long name'
+              expect(some_resource.valid_name?).to be true
+            end
+          end
+
+          context 'IF attribute value length is NOT in length range' do
+            specify 'valid_#{attr}? method returns false' do
+              some_resource.attributes[:name] = ''
+              expect(some_resource.valid_name?).to be false
+              some_resource.attributes[:name] = 's' * 256
+              expect(some_resource.valid_name?).to be false
+            end
+          end
+        end
+
+        context 'FOR Array attribute'
+        context 'FOR Boolean attribute'
+        context 'FOR Integer attribute'
+        context 'FOR Float attribute'
+        context 'FOR DateTime attribute'
       end
 
       describe '#new' do
